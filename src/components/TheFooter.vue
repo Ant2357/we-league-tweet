1885f7a39a5091407f49e7e1e464c13b01b5bb0f
Ant <ryu4979@gmail.com>
2021-11-17 19:44:37 +0900

    fix: faviconの変更 + 著作権表記追加

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,6 +4,7 @@
       <p>
         © 2021 <a href="https://twitter.com/ant2357">ant2357</a>
       </p>
+      <div>© Logo <a href="https://www.designevo.com/jp/" title="無料オンラインロゴメーカー">DesignEvo</a></div>
     </div>
   </footer>
 </template>
